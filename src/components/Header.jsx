383eb8e3042ace90a99654cac5ab60d9be9d76c1
Tanja Schmidt <taschmi@gmx.net>
2023-03-31 22:05:56 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+          <h1>Tanja Schmidt</h1>
+          <p>Web2 & Web3 Developer</p>
+        </header>
+    );
+}
+
+export default Header;
